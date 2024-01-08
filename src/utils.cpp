6e69b999d6ce8caf379a24cb137312a5183a4018
woong137 <woong137137@gmail.com>
2024-01-08 22:31:48 +0900

    Added math utility functions for addition and equality checks

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,21 @@
 #include "math/utils.hpp"
+
+#include <cmath>
+#include <limits>
+#include <stdexcept>
+
+namespace woong137::math {
+auto Add(double lhs, double rhs) -> double {
+  if (std::isnan(lhs) || std::isnan(rhs)) {
+    throw std::invalid_argument("lhs or rhs is NaN");
+  }
+  if (std::isinf(lhs) || std::isinf(rhs)) {
+    throw std::invalid_argument("lhs or rhs is Inf");
+  }
+  return lhs + rhs;
+}
+
+auto IsEqual(double lhs, double rhs) -> bool {
+  return std::fabs(lhs - rhs) < std::numeric_limits<double>::epsilon();
+}
+}  // namespace woong137::math
