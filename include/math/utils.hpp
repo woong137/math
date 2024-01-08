98aca81d36ce671b47742a2f81f9e9987fcf9ace
woong137 <woong137137@gmail.com>
2024-01-08 21:51:41 +0900

    Added skeleton code

---
+++ include/math/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef WOONG137__MATH__UTILS__HPP
+#define WOONG137__MATH__UTILS__HPP
+
+namespace woong137::math {
+  // 사칙연산
+  // Add, Subtract, Multiply, Divide
+
+  // 비교연산
+  // IsEqual
+
+}
+
+#endif
