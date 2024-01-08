c65c4ebc19a2c8e5c8f5bddf68112b996a912fbb
woong137 <woong137137@gmail.com>
2024-01-08 22:34:07 +0900

    Refactored math utils namespace

--- include/math/utils.hpp
+++ include/math/utils.hpp
@@ -2,16 +2,16 @@
 #define WOONG137__MATH__UTILS__HPP
 
 namespace woong137::math {
-  // 사칙연산
-  // Add, Subtract, Multiply, Divide
-  auto Add(double lhs, double rhs) -> double;
-  auto Subtract(double lhs, double rhs) -> double;
-  auto Multiply(double lhs, double rhs) -> double;
-  auto Divide(double lhs, double rhs) -> double;
-  // 비교연산
-  // IsEqual
-  auto IsEqual(double lhs, double rhs) -> bool;
+// 사칙연산
+// Add, Subtract, Multiply, Divide
+auto Add(double lhs, double rhs) -> double;
+auto Subtract(double lhs, double rhs) -> double;
+auto Multiply(double lhs, double rhs) -> double;
+auto Divide(double lhs, double rhs) -> double;
+// 비교연산
+// IsEqual
+auto IsEqual(double lhs, double rhs) -> bool;
 
-}
+}  // namespace woong137::math
 
 #endif
