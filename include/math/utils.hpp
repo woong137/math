86838d799fd9e040b741a47aaecc7b6b605bdd90
woong137 <woong137137@gmail.com>
2024-01-08 22:31:35 +0900

    Added arithmetic and comparison functions to math utils

--- include/math/utils.hpp
+++ include/math/utils.hpp
@@ -4,9 +4,13 @@
 namespace woong137::math {
   // 사칙연산
   // Add, Subtract, Multiply, Divide
-
+  auto Add(double lhs, double rhs) -> double;
+  auto Subtract(double lhs, double rhs) -> double;
+  auto Multiply(double lhs, double rhs) -> double;
+  auto Divide(double lhs, double rhs) -> double;
   // 비교연산
   // IsEqual
+  auto IsEqual(double lhs, double rhs) -> bool;
 
 }
 
