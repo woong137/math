c53be7265995708b3377723755e0bdff7583be6a
woong137 <woong137137@gmail.com>
2024-01-08 22:39:46 +0900

    Added unit tests for Divide function in MathUtils

--- test/unit/utils.cpp
+++ test/unit/utils.cpp
@@ -82,4 +82,32 @@ TEST(MathUtils, Multiply) {
   EXPECT_NE(Multiply(0.5, 0.5), 1.0);
 
 }
+
+TEST(MathUtils, Divide) {
+  EXPECT_THROW(Divide(0.0, std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Divide(std::nan(""), 0.0), std::invalid_argument);
+  EXPECT_THROW(Divide(std::nan(""), std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Divide(0.0, std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+  EXPECT_THROW(Divide(std::numeric_limits<double>::infinity(), 0.0),
+               std::invalid_argument);
+  EXPECT_THROW(Divide(std::numeric_limits<double>::infinity(),
+                      std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+  EXPECT_THROW(Divide(0.0, 0.0), std::invalid_argument);
+  EXPECT_THROW(Divide(1.0, 0.0), std::invalid_argument);
+
+  // 같은 경우
+  EXPECT_EQ(Divide(1.0, 1.0), 1.0);
+  EXPECT_EQ(Divide(2.0, 2.0), 1.0);
+  EXPECT_EQ(Divide(3.0, 2.0), 1.5);
+  EXPECT_EQ(Divide(4.0, 2.0), 2.0);
+
+  // 다른 경우
+  EXPECT_NE(Divide(1.0, 1.0), 0.0);
+  EXPECT_NE(Divide(2.0, 2.0), 0.0);
+  EXPECT_NE(Divide(3.0, 3.0), 0.0);
+  EXPECT_NE(Divide(4.0, 4.0), 0.0);
+}
+
 }  // namespace woong137::math
