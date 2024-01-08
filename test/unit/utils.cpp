c15961098c5d35030fa903dbc5f6539bde9de878
woong137 <woong137137@gmail.com>
2024-01-08 22:37:34 +0900

    Added Multiply test to MathUtils

--- test/unit/utils.cpp
+++ test/unit/utils.cpp
@@ -55,4 +55,31 @@ TEST(MathUtils, Subtract) {
   EXPECT_NE(Subtract(0.5, 0.5), 1.0);
 }
 
+TEST(MathUtils, Multiply) {
+  EXPECT_THROW(Multiply(0.0, std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Multiply(std::nan(""), 0.0), std::invalid_argument);
+  EXPECT_THROW(Multiply(std::nan(""), std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Multiply(0.0, std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+  EXPECT_THROW(Multiply(std::numeric_limits<double>::infinity(), 0.0),
+               std::invalid_argument);
+  EXPECT_THROW(Multiply(std::numeric_limits<double>::infinity(),
+                        std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+
+  // 같은 경우
+  EXPECT_EQ(Multiply(0.0, 0.0), 0.0);
+  EXPECT_EQ(Multiply(1.0, 0.0), 0.0);
+  EXPECT_EQ(Multiply(0.0, 1.0), 0.0);
+  EXPECT_EQ(Multiply(0.5, 0.5), 0.25);
+  EXPECT_EQ(Multiply(2.0, 2.0), 4.0);
+  EXPECT_EQ(Multiply(2.0, 3.0), 6.0);
+
+  // 다른 경우
+  EXPECT_NE(Multiply(0.0, 0.0), 1.0);
+  EXPECT_NE(Multiply(1.0, 0.0), 1.0);
+  EXPECT_NE(Multiply(0.0, 1.0), 1.0);
+  EXPECT_NE(Multiply(0.5, 0.5), 1.0);
+
+}
 }  // namespace woong137::math
