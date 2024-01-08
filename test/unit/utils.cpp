a19bbce85d1074ebcdf4b439f59cd47724a7d188
woong137 <woong137137@gmail.com>
2024-01-08 22:36:14 +0900

    Added unit tests for Subtract function in MathUtils

--- test/unit/utils.cpp
+++ test/unit/utils.cpp
@@ -30,5 +30,29 @@ TEST(MathUtils, Add) {
   EXPECT_NE(Add(0.5, 0.5), 0.0);
 }
 
+TEST(MathUtils, Subtract) {
+  EXPECT_THROW(Subtract(0.0, std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Subtract(std::nan(""), 0.0), std::invalid_argument);
+  EXPECT_THROW(Subtract(std::nan(""), std::nan("")), std::invalid_argument);
+  EXPECT_THROW(Subtract(0.0, std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+  EXPECT_THROW(Subtract(std::numeric_limits<double>::infinity(), 0.0),
+               std::invalid_argument);
+  EXPECT_THROW(Subtract(std::numeric_limits<double>::infinity(),
+                        std::numeric_limits<double>::infinity()),
+               std::invalid_argument);
+
+  // 같은 경우
+  EXPECT_EQ(Subtract(0.0, 0.0), 0.0);
+  EXPECT_EQ(Subtract(1.0, 0.0), 1.0);
+  EXPECT_EQ(Subtract(0.0, 1.0), -1.0);
+  EXPECT_EQ(Subtract(0.5, 0.5), 0.0);
+
+  // 다른 경우
+  EXPECT_NE(Subtract(0.0, 0.0), 1.0);
+  EXPECT_NE(Subtract(1.0, 0.0), 0.0);
+  EXPECT_NE(Subtract(0.0, 1.0), 0.0);
+  EXPECT_NE(Subtract(0.5, 0.5), 1.0);
+}
 
 }  // namespace woong137::math
